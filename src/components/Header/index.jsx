169ccbbc815f4c3a268395d1272c58a16d794a4f
Rafael Gerônimo <rafael@geronimo.dev>
2021-11-04 07:33:26 -0300

    feat: header style and navigation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,27 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/auth';
+import styles from './styles.module.scss';
 
 function Header() {
-  const { userData } = useContext(AuthContext);
+  const { userData, signOut } = useContext(AuthContext);
   const { name } = userData;
   return (
-    <header>
+    <header className={ styles.header }>
+      <div>
       Olá { name }
+      </div>
+      <nav className={ styles.navmenu }>
+        <Link to='/tasks'>
+          Tarefas
+        </Link>
+        <Link to='/profile'>
+          Perfil
+        </Link>
+        <Link to='/' onClick={ signOut }>
+          Sair
+        </Link>
+      </nav>
     </header>
   );
 }
--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/auth";
+import styles from './styles.module.scss';
 
 function UserProfile() {
   const { userData, signOut } = useContext(AuthContext);
   return (
     <section>
-      <h1>User</h1>
+      <h1>{ userData.name }</h1>
+      <img src={ userData.avatar } alt="Avatar" className={ styles.avatar } />
       <Link to="/">
         <button onClick={ signOut }>
           Sair
