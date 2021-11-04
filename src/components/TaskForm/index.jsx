45d82e6c2810751c9f6cc6eca918b2d1dd51a13b
Rafael Gerônimo <rafael@geronimo.dev>
2021-11-04 17:32:53 -0300

    style: task form styles

--- src/components/TaskForm/index.jsx
+++ src/components/TaskForm/index.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import api from "../../services/api";
 
+import styles from './styles.module.scss';
+
 const TaskForm = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
@@ -22,53 +24,65 @@ const TaskForm = () => {
       .catch(error => {
         console.log(error.response);
       })
-    // const {  }
   }
 
   return (
-    <form
-      action=""
-      onSubmit={ (e) => (e.preventDefault(),
-        registerTask(title, description, initialDate, endDate, taskStatus)) }
-    >
-      <input
-        type="text"
-        placeholder="Título da tarefa"
-        onChange={ (e) => setTitle(e.target.value) }
-      />
-      <textarea
-        name="description"
-        type="text"
-        placeholder="Descrição"
-        autoComplete="off"
-        onChange={ (e) => setDescription(e.target.value) }
-      ></textarea>
-      Data inicial:
-      <input
-        name="initialDate"
-        type="date"
-        onChange={ (e) => setInitialDate(e.target.value) }
-      />
-      Data final:
-      <input
-        name="finalDate"
-        type="date"
-        onChange={ (e) => setEndDate(e.target.value) }
-      />
-      <label htmlFor="taskselect">Situação: </label>
-      <select
-        name=""
-        onChange={ (e) => setTaskStatus(e.target.value) }
-      >
-        <option value="todo" selected>A fazer</option>
-        <option value="doing">Em andamento</option>
-        <option value="done">Concluído</option>
-      </select>
-      <input
-        type="submit"
-        value="Cadastrar tarefa"
-      />
-    </form>
+    <div className={ styles.taskFormComponent }>
+        <h1>Adicione uma tarefa:</h1>
+      <form
+        className={ styles.taskForm }
+        action=""
+        onSubmit={ (e) => (e.preventDefault(),
+          registerTask(title, description, initialDate, endDate, taskStatus)) }
+          >
+        <input
+          type="text"
+          placeholder="Título da tarefa"
+          onChange={ (e) => setTitle(e.target.value) }
+          />
+        {/* <textarea
+          name="description"
+          type="text"
+          placeholder="Descrição"
+          autoComplete="off"
+          onChange={ (e) => setDescription(e.target.value) }
+        ></textarea> */}
+        <div className={ styles.taskDateFields }>
+          <label className={ styles.dateLabel } htmlFor="initialDate">
+            <p>Data inicial:</p>
+            <input
+              name="initialDate"
+              type="date"
+              onChange={ (e) => setInitialDate(e.target.value) }
+              />
+          </label>
+
+          <label className={ styles.dateLabel } htmlFor="endDate">
+            <p>Data final:</p>
+            <input
+              name="finalDate"
+              type="date"
+              onChange={ (e) => setEndDate(e.target.value) }
+              />
+          </label>
+        </div>
+
+        <label htmlFor="taskselect">Situação: </label>
+        <select
+          name=""
+          defaultValue="todo"
+          onChange={ (e) => setTaskStatus(e.target.value) }
+          >
+          <option value="todo">Pendente</option>
+          <option value="doing">Em andamento</option>
+          <option value="done">Pronto</option>
+        </select>
+        <input
+          type="submit"
+          value="Cadastrar tarefa"
+          />
+      </form>
+    </div>
   );
 }
 
--- src/components/TasksTable/index.jsx
+++ src/components/TasksTable/index.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
+// import { Modal, Button } from "react-bootstrap";
+import styles from './styles.module.scss';
+import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
 
 import api from '../../services/api';
 
 const TasksTable = () => {
   const { userData } = useContext(AuthContext);
   const userId = userData.id;
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
+  const [currentSort, setCurrentSort] = useState('default');
+  // const [modalTaskData, setModalTaskData] = useState();
+  // const [taskModalShow, setTaskModalShow] = useState(false);
+  // const [modalShow, setModalShow] = useState(false);
+
   useEffect(() => {
     const token = localStorage.getItem('@today:token');
     if (token) {
@@ -18,7 +26,34 @@ const TasksTable = () => {
         console.log(error.response);
       });
     }
-  }, []);
+  });
+
+  // function TaskModal(props, task) {
+  //   console.log(props);
+  //   return (
+  //     <Modal
+  //       {...props}
+  //       size="lg"
+  //       aria-labelledby="contained-modal-title-vcenter"
+  //       centered
+  //     >
+  //       <Modal.Header closeButton>
+  //         <Modal.Title id="contained-modal-title-vcenter">
+  //           { task.title }
+  //         </Modal.Title>
+  //       </Modal.Header>
+  //       <Modal.Body>
+  //         <h4>Centered Modal</h4>
+  //         <p>
+  //           { task.title }
+  //         </p>
+  //       </Modal.Body>
+  //       <Modal.Footer>
+  //         <Button onClick={props.onHide}>Close</Button>
+  //       </Modal.Footer>
+  //     </Modal>
+  //   )
+  // }
 
   const columns = [
     {
@@ -34,28 +69,80 @@ const TasksTable = () => {
       title: 'Situação'
     }
   ];
+
+  // function RenderModal(task) {  
+  //   return (
+  //     <>
+  //       <Button variant="primary" onClick={() => setModalShow(true)}>
+  //         Carregar modal
+  //       </Button>
+  //       <TaskModal
+  //         show={modalShow}
+  //         onHide={() => setModalShow(false)}
+  //       ></TaskModal>
+  //     </>
+  //   )
+  // }
+
+  // function showModal(task) {
+  //   setModalShow(true);
+  //   // console.log(task);
+  //   const modalProps = {show: modalShow, onHide: setModalShow(false)};
+  //   TaskModal(modalProps, task);
+    
+    
+  //   // <TaskModal
+  //   //   show={modalShow}
+  //   //   onHide={() => setModalShow(false)}
+  //   // >
+  //   // </TaskModal>
+  // }
+
+  const sortTypes = {
+    up: {
+      class: 'sort-up',
+      fn: (a, b) => console.log(a.initialDate)
+    },
+    down: {
+      class: 'sort-down',
+      fn: (a, b) => b.initialDate - a.initialDate
+    },
+    default: {
+      class: 'sort',
+      fn: (a, b) => a
+    }
+  };
+
+  const changeSort = () => {
+    if (currentSort === 'down') setCurrentSort('up')
+    else if (currentSort === 'up') setCurrentSort('default')
+    else if (currentSort === 'default') setCurrentSort('down');
+  };
+
   return (
     <>
+      {/* <RenderModal /> */}
       { tasks.length === 0 ? 
         <div>
           <p>Não há tarefas cadastradas para {userData.name}.</p>
           <p>Utilize o formulário acima para cadastrar suas tarefas.</p>
         </div> : 
-        <table>
+        <table className="sortable">
           <thead>
             <tr>
               { columns.map((column) => (
                 <th>
                   { column.title }
+                  <AiFillCaretUp onClick={() => changeSort()} />
                 </th>
               )) }
             </tr>
           </thead>
           <tbody>
-              { tasks.map((task) => (
-                <tr key={ task.id }>
+              { tasks.sort(sortTypes[currentSort].fn).map((task) => (
+                <tr key={ task._id }/*  onClick={ () => showModal(task) } */>
                     <td>
-                      { task.title }
+                        { task.title }
                     </td>
                     <td>
                       { task.initialDate }
